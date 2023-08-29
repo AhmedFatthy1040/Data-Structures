8815ffd0481f5bcd57078ed6489698968d1e0d04
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-08-29 07:25:18 +0300

    Implemented keys method in HashTable.java class, and Tested it in Main.java class.

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main {
         ht.set("first", 5);
         ht.set("second", 7);
         ht.set("third", 1);
+        ht.set("fourth", 3);
+        ht.set("fifth", 9);
+        ht.set("sixth", 4);
         ht.printTable();
         System.out.println("\n" + ht.get("first"));
+        System.out.println(ht.keys());
     }
 }
