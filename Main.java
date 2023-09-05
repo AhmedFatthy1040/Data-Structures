1bfc566de8033aeea57e4012df063c6003326df3
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-09-05 17:54:25 +0300

    Made a complete Graph.java class , and tested it in Main.java.

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        HashTable ht = new HashTable();
-        ht.set("first", 5);
-        ht.set("second", 7);
-        ht.set("third", 1);
-        ht.set("fourth", 3);
-        ht.set("fifth", 9);
-        ht.set("sixth", 4);
-        ht.printTable();
-        System.out.println("\n" + ht.get("first"));
-        System.out.println(ht.keys());
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "C");
+        graph.addEdge("A", "D");
+        graph.addEdge("C", "D");
+        graph.removeEdge("A", "B");
+        graph.removeVertex("D");
+        graph.printGraph();
     }
 }
